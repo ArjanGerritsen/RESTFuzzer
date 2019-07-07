6b67dff4bcd2881d422910c844f4bd001fb74ca8
Arjan Gerritsen <email@arjangerritsen.nl>
2019-07-07 20:24:02 +0200

    Begin gemaakt met database (MySQL) connection.

--- fuzz-rest-service/src/main/java/nl/ou/se/fuzz/rest/service/controller/rest/projects/ProjectsController.java
+++ fuzz-rest-service/src/main/java/nl/ou/se/fuzz/rest/service/controller/rest/projects/ProjectsController.java
@@ -4,15 +4,21 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import nl.ou.se.fuzz.rest.service.dao.projects.ProjectsService;
+
 @RestController()
 @RequestMapping("/rest/projects")
 public class ProjectsController {
+
+	@Autowired
+	ProjectsService projectsService;
 
 	private List<ProjectDto> projects = new ArrayList<ProjectDto>();
 
---
+++ fuzz-rest-service/src/main/java/nl/ou/se/fuzz/rest/service/dao/projects/ProjectsService.java
@@ -0,0 +1,7 @@
+package nl.ou.se.fuzz.rest.service.dao.projects;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface  ProjectsService extends CrudRepository<Project, Integer>  {
+
+}
