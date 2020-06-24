023b8ccdd1a00a77e372acdde3680b8a2aa39f1d
Arjan Gerritsen <email@arjangerritsen.nl>
2020-06-24 16:03:03 +0200

    Begin gemaakt met dictionary fuzzer.

---
+++ rest-fuzzer/backend/src/main/java/nl/ou/se/rest/fuzzer/components/fuzzer/type/FuzzerModelBasedDictionary.java
@@ -0,0 +1,28 @@
+package nl.ou.se.rest.fuzzer.components.fuzzer.type;
+
+import java.util.Map;
+
+import nl.ou.se.rest.fuzzer.components.data.fuz.domain.FuzProject;
+import nl.ou.se.rest.fuzzer.components.data.task.domain.Task;
+import nl.ou.se.rest.fuzzer.components.fuzzer.metadata.MetaDataUtil;
+import nl.ou.se.rest.fuzzer.components.fuzzer.metadata.MetaDataUtil.Meta;
+
+public class FuzzerModelBasedDictionary extends FuzzerBase implements Fuzzer {
+
+    // variable(s)
+    private FuzProject project = null;
+    private MetaDataUtil metaDataUtil = null;
+
+    // constructor(s)
+    
+    // method(s)
+    public void start(FuzProject project, Task task) {
+        this.project = project;
+
+    }
+
+    public Boolean isMetaDataValid(Map<String, Object> metaDataTuples) {
+        this.metaDataUtil = new MetaDataUtil(metaDataTuples);
+        return metaDataUtil.isValid(Meta.CONFIGURATION);
+    }
+}
